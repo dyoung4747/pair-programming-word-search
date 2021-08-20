ec0d8e5774f0e4bcd445b9784b4e2278fb4c37af
Douglas Young <dyoung4747@hotmail.com>
2021-08-20 17:49:43 +0000

    created tests for vertical/empty array/empty string cases

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is horizontally present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,44 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is vertically present", function() {
+    const result = wordSearch([
+      ['B', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['U', 'E', 'I', 'N', 'F', 'E', 'L', 'C'],
+      ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['D', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['U', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['I', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['N', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['G', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'BUILDUING')
+
+    assert.isTrue(result);
+  });
+
+  it("should return undefined if passed an empty array", function() {
+    const result = wordSearch([
+    ], 'BUILDUING')
+
+    assert.isUndefined(result);
+  });
+
+  it("should return undefined if the word is an empty string", function() {
+    const result = wordSearch([
+      ['B', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['U', 'E', 'I', 'N', 'F', 'E', 'L', 'C'],
+      ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['D', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['U', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['I', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['N', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['G', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '')
+
+    assert.isUndefined(result);
   });
 });
