d10b4a2da53a0e994620e95b25c0da5fc4405b54
Douglas Young <dyoung4747@hotmail.com>
2021-08-20 17:48:50 +0000

    debugged wordsearch/added code for vertical words

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    const result = Array.from({length: matrix[0].length}, () => new Array(matrix.length).fill(0));
+    for (let r = 0; r < matrix.length; r++) {
+        for (let c = 0; c < matrix[r].length; c++) {
+        result[c][r] = matrix[r][c];
+        }
     }
-}
+    return result;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+
+    if(letters.length === 0) return undefined;
+
+    if(word.length === 0) return undefined;
+
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    let isTrue = false;
+
+    const verticalArr = transpose(letters);
+    const verticalJoin = verticalArr.map(ls => ls.join(''));
+    // console.log(verticalJoin)
+
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return isTrue = true;
+        }
+    }
+
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return isTrue = true;
+        }
+    }
+
+    return isTrue;
+};
+
+module.exports = wordSearch;
